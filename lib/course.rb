d29ccd8d38d68dde432dea83c43b9e4f2764f33e
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-08-31 14:27:21 -0400

    commit tree 76cc2c4897d9f163e18965dedeb5341bdf68eba8

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,27 @@
 
+require 'pry'
 
+class Course
+
+    @@all = []
+
+    attr_accessor :title, :schedule, :description
+
+    def initialize (title: , schedule: , description:)
+        @title = title
+        @schedule = schedule
+        @description = description
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+        @@all.clear
+    end
+
+end
+
+binding.pry
